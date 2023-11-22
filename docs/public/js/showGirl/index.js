9caf2f21aa25fb46189532de567b35d3fc861635
gujinzhu <15119376155@163.com>
2023-11-22 17:43:15 +0800

    feat: add onload

--- docs/public/js/showGirl/index.js
+++ docs/public/js/showGirl/index.js
@@ -2,24 +2,26 @@ const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
   navigator.userAgent
 )
 
-live2d.init({
-  model: {
-    // jsonPath: 'https://unpkg.com/live2d-widget-model-chitose@1.0.5/assets/chitose.model.json',
-    // jsonPath: 'https://unpkg.com/live2d-widget-model-haruto@1.0.5/assets/haruto.model.json'
-    // jsonPath: 'https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json',
-    // jsonPath: 'https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json',
-    jsonPath: 'https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json',
-  },
-  display: {
-    superSample: 2,
-    width: isMobile ? 120 : 280,
-    height: isMobile ? 160 : 360,
-    position: isMobile ? 'right' : 'left',
-    hOffset: 0,
-    vOffset: 0,
-  },
-  mobile: {
-    show: true,
-    scale: 1,
-  },
+window.addEventListener('load', () => {
+  live2d.init({
+    model: {
+      // jsonPath: 'https://unpkg.com/live2d-widget-model-chitose@1.0.5/assets/chitose.model.json',
+      // jsonPath: 'https://unpkg.com/live2d-widget-model-haruto@1.0.5/assets/haruto.model.json'
+      // jsonPath: 'https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json',
+      // jsonPath: 'https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json',
+      jsonPath: 'https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json',
+    },
+    display: {
+      superSample: 2,
+      width: isMobile ? 120 : 280,
+      height: isMobile ? 160 : 360,
+      position: isMobile ? 'right' : 'left',
+      hOffset: 0,
+      vOffset: 0,
+    },
+    mobile: {
+      show: true,
+      scale: 1,
+    },
+  })
 })
