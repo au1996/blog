97d5ee205892a7585c3e593144c930a5eb39dea6
au1996 <au1996@users.noreply.github.com>
2024-07-17 02:47:07 +0000

    deploy d2d5380dd6c06b529b95b1bec9e00abb94028905

---
+++ assets/extend_index.md.DjbjANgf.js
@@ -0,0 +1,6 @@
+import{aw as a,q as s,p as e,aR as n}from"./chunks/framework.tTD5oJD3.js";const m=JSON.parse('{"title":"汇总一些计算机相关知识","description":"","frontmatter":{},"headers":[],"relativePath":"extend/index.md","filePath":"extend/index.md","lastUpdated":1721184377000}'),t={name:"extend/index.md"},p=n(`<h1 id="汇总一些计算机相关知识" tabindex="-1">汇总一些计算机相关知识 <a class="header-anchor" href="#汇总一些计算机相关知识" aria-label="Permalink to &quot;汇总一些计算机相关知识&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>吾生也有涯</span></span>
+<span class="line"><span>而知也无涯</span></span>
+<span class="line"><span>以有涯随无涯</span></span>
+<span class="line"><span>殆已!</span></span>
+<span class="line"><span>已而为知者</span></span>
+<span class="line"><span>殆而已矣!</span></span></code></pre></div><blockquote><p>人的生命有限，而知识无穷，以有限的生命去追求无穷的知识，就会精疲力竭，既然如此，还去追求知识的人，就只能弄得疲困了。做什么事都不要绝对化，要适可而止。永远有你达不到的东西，不必苛求。</p></blockquote>`,3),i=[p];function o(c,l,d,r,_,h){return e(),s("div",null,i)}const x=a(t,[["render",o]]);export{m as __pageData,x as default};
